2b9c098d18d9e500abc0a90cbedb5fde45976131
althaf <amalthaf111@gmail.com>
2026-07-31 11:45:48 +0530

    Remove Sticky CTA component and update styles for footer and hero tag

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Process from './components/Process';
 import CtaBand from './components/CtaBand';
 import Booking from './components/Booking';
 import Footer from './components/Footer';
-import StickyCta from './components/StickyCta';
 
 export default function App() {
   return (
@@ -25,7 +24,6 @@ export default function App() {
         <Booking />
       </main>
       <Footer />
-      <StickyCta />
     </>
   );
 }
